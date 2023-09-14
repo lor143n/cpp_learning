b88cd811239fde162fa339df7d690cd96c4c75a2
lor3n <lorenzo.piarulli3000@gmail.com>
2023-09-14 13:41:22 +0200

    Vertex Buffer and Vertex Attributes

--- src/main.cpp
+++ src/main.cpp
@@ -32,15 +32,35 @@ int main(){
         return -1;
     }
 
-    
-    glGenBuffers(1, )
+    //Vertex che vogliamo memorizzare ed utilizzare
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f, 0.5f,
+         0.5f, -0.5f
+    };
+
+    /*
+    Creazione di un buffer (blocco di memoria riservata), selezione del buffer da usare (binding), allocazione dei dati
+    */
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+    //Every attribute has to be enabled (OpenGL works has a state machine so not matter the order)
+    glEnableVertexAttribArray(0);
+    //Every vertex has a bunch of attributes, in this case we only the position
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
     // Ciclo principale della finestra
     while (!glfwWindowShouldClose(window)) {
         
+        glClear(GL_COLOR_BUFFER_BIT);
 
-
+        //questa e' la draw call del buffer selezionato (binded)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Swap dei buffer
         glfwSwapBuffers(window);
